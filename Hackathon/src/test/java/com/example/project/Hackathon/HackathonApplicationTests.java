8066d4442698721a3aa2b6681384ee89413db86c
akash-mulge <akash.mulge@zebra.com>
2022-10-12 00:41:21 +0530

    Project Commit

---
+++ Hackathon/src/test/java/com/example/project/Hackathon/HackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project.Hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
