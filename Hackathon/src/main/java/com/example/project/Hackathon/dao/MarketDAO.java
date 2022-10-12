9c76a0c49dbf93f648cd7b86df4d9f94dc515b96
akash-mulge <akash.mulge@zebra.com>
2022-10-12 11:56:42 +0530

    Dao Updated

--- Hackathon/src/main/java/com/example/project/Hackathon/dao/MarketDAO.java
+++ Hackathon/src/main/java/com/example/project/Hackathon/dao/MarketDAO.java
@@ -11,6 +11,8 @@ import com.example.project.Hackathon.model.Market;
 public interface MarketDAO{
 
 	 List<Stock> getListOfStocksForShareHolder(int Id,Date effDate,Date endDate);
+	 
+	 ShareHolder getShareHolderAndStocks(int shareHolderId,Date effDate,Date endDate);
 	
 	
 }
--- Hackathon/src/main/java/com/example/project/Hackathon/dao/MarketDAOImpl.java
+++ Hackathon/src/main/java/com/example/project/Hackathon/dao/MarketDAOImpl.java
@@ -9,6 +9,7 @@ import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
+import com.example.project.Hackathon.model.ShareHolder;
 import com.example.project.Hackathon.model.Stock;
 
 @Repository
@@ -22,9 +23,18 @@ public class MarketDAOImpl implements MarketDAO {
 	@Override
 	public List<Stock> getListOfStocksForShareHolder(int Id, Date effDate, Date endDate) {
 		String query = "select s from Stock s where selectedShareHolder.shareHolderId=:Id and date BETWEEN :effDate and :endDate";
-		TypedQuery<Stock> stocks = entityManager.createQuery(query, Stock.class).setParameter("shareHolderId", Id).setParameter("startDate", effDate).setParameter("endDate", endDate);
+		TypedQuery<Stock> stocks = entityManager.createQuery(query, Stock.class).setParameter("Id", Id).setParameter("startDate", effDate).setParameter("endDate", endDate);
 		return stocks.getResultList();
 		
+	}
+
+
+
+	@Override
+	public ShareHolder getShareHolderAndStocks(int shareHolderId, Date effDate, Date endDate) {
+		String query = "select s from ShareHolder s where shareHolderId=:shareHolderId and date BETWEEN :effDate and :endDate";
+		TypedQuery shareHolder = entityManager.createQuery(query, Stock.class).setParameter("shareHolderId", shareHolderId).setParameter("startDate", effDate).setParameter("endDate", endDate);
+		return (ShareHolder) shareHolder.getSingleResult();
 	}
 
 }
